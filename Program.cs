d2fe9186acec2234d33dd95998195d676c86204e
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2025-10-23 11:38:21 +0700

    feat: implement the core functionality
    Add project to the solution file.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Sharprompt;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelKeyPressHandler);
+
+        while (true)
+        {
+            int days = Prompt.Input<int>("Days ago?");
+            var ts = TimeSpan.FromDays(days);
+            var thatDate = DateTime.Today - ts;
+            var output = thatDate.ToString("d");
+
+            Console.WriteLine(output);
+        }
+    }
+
+    protected static void CancelKeyPressHandler(object sender, ConsoleCancelEventArgs args)
+    {
+        Console.WriteLine("\nBye!");
+        args.Cancel = true;
+        Environment.Exit(0);
+    }
+}
