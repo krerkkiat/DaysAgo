c926af81f99ef2c342420ae966caabc556ba0b45
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2025-11-06 11:40:02 +0700

    feat: add --days option
    To allow for a non-interactive call (in this case from Espanso).

--- Program.cs
+++ Program.cs
@@ -1,19 +1,39 @@
-﻿using Sharprompt;
+﻿using CommandLine;
+using Sharprompt;
+using System.Runtime.InteropServices;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelKeyPressHandler);
+        var parserResult = CommandLine.Parser.Default.ParseArguments<Options>(args);
+        await parserResult.WithParsedAsync(RunOptions);
+    }
 
-        while (true)
+    public static async Task RunOptions(Options opts)
+    {
+        if (opts.Days is null)
         {
-            int days = Prompt.Input<int>("Days ago?");
-            var ts = TimeSpan.FromDays(days);
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelKeyPressHandler);
+
+            while (true)
+            {
+                int days = Prompt.Input<int>("Days ago?");
+                var ts = TimeSpan.FromDays(days);
+                var thatDate = DateTime.Today - ts;
+                var output = thatDate.ToString("d");
+
+                Console.WriteLine(output);
+            }
+        }
+        else
+        {
+            var ts = TimeSpan.FromDays(opts.Days ?? 0);
             var thatDate = DateTime.Today - ts;
             var output = thatDate.ToString("d");
 
             Console.WriteLine(output);
+            
         }
     }
 
@@ -23,4 +43,10 @@ public class Program
         args.Cancel = true;
         Environment.Exit(0);
     }
+}
+
+public class Options
+{
+    [Option(shortName: 'd', longName: "days", HelpText = "Number of days to use")]
+    public int? Days { get; set; }
 }
